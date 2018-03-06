53735e780f5c347e42553ed17241cb438c7edb67
freddypauls <paasan-fred@hotmail.com>
2018-03-06 16:06:08 +0100

    2

--- app/src/main/java/no/ntnu/fredrik/lab2/UserPreferance.java
+++ app/src/main/java/no/ntnu/fredrik/lab2/UserPreferance.java
@@ -1,7 +1,12 @@
 package no.ntnu.fredrik.lab2;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class UserPreferance extends AppCompatActivity {
 
@@ -9,5 +14,60 @@ public class UserPreferance extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_preferance);
+
+        Spinner spinner = findViewById(R.id.spinner);
+        Spinner spinner2 = findViewById(R.id.spinner2);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.list_amount, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.refresh_time, android.R.layout.simple_spinner_item);
+
+
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        spinner2.setAdapter(adapter2);
+
+        // Shared preference, to save the Spinner variable
+        final SharedPreferences spin = getSharedPreferences("Lab2", MODE_PRIVATE);
+        spinner.setSelection(spin.getInt("list_amount", 0));
+
+        // Spinner select
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> AdapterView, View view, int i, long j) {
+                SharedPreferences.Editor editor = spin.edit();
+                editor.putInt("list_amount", i);
+                editor.commit();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+
+
+        });
+
+        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> AdapterView, View view, int i, long j) {
+                        SharedPreferences.Editor editor = spin.edit();
+                        editor.putInt("refresh_time", i);
+                        editor.commit();
+
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+
+                    }
+        });
     }
 }
