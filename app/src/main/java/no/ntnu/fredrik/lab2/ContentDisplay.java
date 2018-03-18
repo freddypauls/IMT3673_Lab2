a10391337f757da2ac477e068f365868ef9787a3
freddypauls <paasan-fred@hotmail.com>
2018-03-18 16:39:37 +0100

    Basically done, might do more
    Should be finished, practically started over from the last commit (alot of the same but I had to change because of some file errors), also didnt commit the last few fimes I worked

--- app/src/main/java/no/ntnu/fredrik/lab2/ContentDisplay.java
+++ app/src/main/java/no/ntnu/fredrik/lab2/ContentDisplay.java
@@ -2,6 +2,7 @@ package no.ntnu.fredrik.lab2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class ContentDisplay extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class ContentDisplay extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content_display);
+
+        //Gets url from listView activity
+        String webPageUrlFromRSS = getIntent().getStringExtra("Listing");
+
+        //Starts a new web view with the link sent from listView
+        WebView webview = new WebView(this);
+        setContentView(webview);
+        webview.loadUrl(webPageUrlFromRSS);
     }
 }
